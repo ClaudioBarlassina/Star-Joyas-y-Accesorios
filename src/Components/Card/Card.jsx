e99f5016ddaf817c03d4f47b1f32d35675a31ecb
ClaudioBarlassina <claudio_barlassina_ar@hotmail.com>
2024-09-04 00:00:50 -0300

    act-04-09-24

--- src/Components/Card/Card.jsx
+++ src/Components/Card/Card.jsx
@@ -1,10 +1,36 @@
-import React from 'react'
-import "./Card.css"
+import React from "react";
+import "./Card.css";
+import im from "../../assets/zapatillas.webp";
 
 function Card() {
   return (
-    <div className='container'>Card</div>
-  )
+    <div className="conteiner-primario">
+      <div className="ico">icono</div>
+      <div className="conteiner-image">
+        <img className="image" src={im} alt="" />
+      </div>
+      <div className="description">
+        <h3>Producto</h3>
+        <p className="descr-title">
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Culpa
+        </p>
+        <div className="conteiner-color-impor">
+          <div className="color">
+            <h3 className="col"> Color:</h3>
+            <input type="radio" name="Rojo" id="" />
+            <input type="radio" name="Azul" id="" />
+            <input type="radio" name="Gris" id="" />
+          </div>
+          <div className="cont-import">
+            <h4 className="import">$09.00</h4>
+          </div>
+        </div>
+        <div className="Conj-button">
+          <button className="button">Add To Cart</button>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Card
+export default Card;
--- src/Components/Contenido/Contenido.jsx
+++ src/Components/Contenido/Contenido.jsx
@@ -1,13 +1,28 @@
-import React from 'react'
-import "./Contenido.css"
+import React from "react";
+import "./Contenido.css";
 import Card from "../Card/Card"
+import contenido from "../../init/contenido.json"
 
 function Contenido() {
   return (
-    <div className='contenido-container'>
-      <Card/>
+    <div className="contenido-container">
+        <h4>LO MAS BUSCADO</h4>
+      <div className="Contenido-primary">
+        {/* {contenido.map{
+
+        }} */}
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+       
+      </div>
     </div>
-  )
+  );
 }
 
-export default Contenido
+export default Contenido;
