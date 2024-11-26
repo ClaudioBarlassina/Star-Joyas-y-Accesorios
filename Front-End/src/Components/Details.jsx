038cf4cd3f505256d6cc62c82b50565c463f7176
ClaudioBarlassina <claudio_barlassina_ar@hotmail.com>
2024-11-25 22:23:15 -0300

    actualizacion-details

--- Front-End/src/Components/Details.jsx
+++ Front-End/src/Components/Details.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { useLocation, useParams } from 'react-router-dom'
 
 import { Link } from 'react-router-dom';
+import "../Components/Details.css"
 
 const Details = () => {
 
@@ -13,22 +14,27 @@ const Details = () => {
   const dato = datos.find((item) => item.id === id)
   
   return (
-    <div>
+    <div className='conteiner-general'>
+      <div className='details-organizacion'>
+
       <Link to={"/Home" }>
       
       <button>Volver</button>
       </Link>
-      <h4>Detalle del Producto</h4>
-      <img src={dato.image } alt=""  />
-      <h2>{dato.nombre}</h2>
-      <h4>{dato.material}</h4>
-      <h4>{dato.categoria }</h4>
-      <h4>{dato.precio}</h4>
-      <p>{dato.Descripcion }</p>
+      <h4 className='details-titulo'>Detalle del Producto</h4>
+      </div>
+      
+      <div className='details-conteiner'>
+      <img src={dato.image } alt="" className='details-img'  />
+      <h2 className='details-nombre'>{dato.nombre}</h2>
+      <h4 className="details-material">Material: {dato.material}</h4>
+      <h4 className='details-categoria'>Categoria: {dato.categoria }</h4>
+      <h4 className='details-precio'> $ {dato.precio}</h4>
+      <p className='details-descripcion'>{dato.Descripcion }</p>
 
         <button>Agregar al carrito</button>     
 
-          
+        </div>   
     </div>
   )
 }
