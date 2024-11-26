b9e7781697d54a8f8c489fca2f3e9d3d5334e367
ClaudioBarlassina <claudio_barlassina_ar@hotmail.com>
2024-11-26 10:41:24 -0300

    .

--- Front-End/src/Components/Landing.jsx
+++ Front-End/src/Components/Landing.jsx
@@ -1,26 +1,19 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import { useCart } from '../Hook/useCart'
-import portada from "../Utils/portada-web-676x1024.webp"
-import Menu from "../Components/Menu"
+import React from "react";
+import { Link } from "react-router-dom";
+import "../Components/Landing.css"
+import portada from "../Utils/portada-web-676x1024.webp";
+import Menu from "../Components/Menu";
+
 const Landing = () => {
-
-
-
-
   return (
     <div>
-
-    <Menu/>
-      <img src={portada } alt="" />
+      <Menu />
+      <img src={portada} alt="" className="img-landing" />
       <Link to={"/Home"}>
-        <button >ingresar</button>
-       
-     
+        <button>ingresar</button>
       </Link>
-
     </div>
-  )
-}
+  );
+};
 
-export default Landing
+export default Landing;
